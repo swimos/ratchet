303dc569806ba1d12c6cfdc2f9226608ab1f8396
SirCipher <tklapwijk92@gmail.com>
2024-05-10 17:02:14 +0100

    Factors out common autobahn code

--- ratchet_rs/autobahn/client/src/main.rs
+++ ratchet_rs/autobahn/client/src/main.rs
@@ -1,36 +1,12 @@
 use std::env::current_dir;
-use std::fs::File;
-use std::io::BufReader;
 use std::process::Stdio;
-use std::time::Duration;
 
-use anyhow::{bail, Context, Result};
-use serde_json::Value;
-use tokio::net::TcpStream;
+use anyhow::{Context, Result};
 use tokio::process::Command;
-use tokio::time::Instant;
 
-use ratchet_rs::{subscribe, WebSocketConfig};
+use utils::{await_server_start, cargo_command, validate_results};
 
 const PWD_ERR: &str = "Failed to get PWD";
-
-fn cargo_command() -> Result<Command> {
-    let mut pwd = current_dir().context(PWD_ERR)?;
-    pwd.push("ratchet_rs");
-
-    let mut cmd = Command::new("cargo");
-    cmd.args(&[
-        "run",
-        "--release",
-        "--example",
-        "autobahn-client",
-        "--features",
-        "deflate",
-    ])
-    .current_dir(pwd);
-
-    Ok(cmd)
-}
 
 async fn kill_container() {
     Command::new("docker")
@@ -89,88 +65,20 @@ async fn main() -> Result<()> {
             .await
     });
 
-    await_server_start().await?;
+    await_server_start(9001).await?;
 
-    cargo_command()?
+    cargo_command("autobahn-client")?
         .spawn()
         .context("Failed to spawn docker container")?
         .wait()
         .await
         .context("Failed to run autobahn client")?;
 
-    validate_results()?;
+    let mut results_dir = current_dir().context(PWD_ERR)?;
+    results_dir.push("ratchet_rs/autobahn/client/results");
+
+    validate_results(results_dir)?;
     kill_container().await;
 
     Ok(())
-}
-
-fn validate_results() -> Result<()> {
-    let mut results_file = current_dir().context(PWD_ERR)?;
-    results_file.push("ratchet_rs/autobahn/client/results/index.json");
-
-    let file = File::open(results_file).context("Failed to open client results file")?;
-    let reader = BufReader::new(file);
-    let results = serde_json::from_reader::<_, Value>(reader)?;
-
-    match results {
-        Value::Object(map) => {
-            let ratchet_results = map["Ratchet"]
-                .as_object()
-                .expect("Invalid result structure");
-
-            let mut failures = Vec::new();
-
-            for (test_id, test) in ratchet_results {
-                match test {
-                    Value::Object(object) => match object["behavior"].as_str() {
-                        Some(result) if result == "OK" => {}
-                        Some(result) => {
-                            failures.push(format!("Test {test_id} failed with: {result}"));
-                        }
-                        _ => bail!("Invalid results structure"),
-                    },
-                    _ => bail!("Invalid results structure"),
-                }
-            }
-
-            if !failures.is_empty() {
-                println!("Test suite encountered failures:");
-
-                for test in failures {
-                    println!("\t{test}");
-                }
-            }
-        }
-        _ => bail!("Invalid results structure"),
-    }
-
-    Ok(())
-}
-
-async fn await_server_start() -> Result<()> {
-    const TIMEOUT: u64 = 30;
-    let start = Instant::now();
-
-    loop {
-        match await_handshake().await {
-            Ok(()) => break Ok(()),
-            Err(_) => {
-                if start.elapsed() > Duration::from_secs(TIMEOUT) {
-                    bail!("Autobahn Server failed to start within {TIMEOUT} seconds");
-                }
-                tokio::time::sleep(Duration::from_secs(1)).await;
-            }
-        }
-    }
-}
-
-async fn await_handshake() -> std::result::Result<(), ratchet_rs::Error> {
-    let stream = TcpStream::connect("127.0.0.1:9001").await?;
-    subscribe(
-        WebSocketConfig::default(),
-        stream,
-        "ws://localhost:9001/getCaseCount",
-    )
-    .await
-    .map(|_| ())
 }
--- ratchet_rs/autobahn/server/src/main.rs
+++ ratchet_rs/autobahn/server/src/main.rs
@@ -1,35 +1,10 @@
 use std::env::current_dir;
-use std::fs::File;
-use std::io::BufReader;
-use std::time::Duration;
 
 use anyhow::{bail, Context, Result};
-use serde_json::Value;
-use tokio::net::TcpStream;
 use tokio::process::Command;
-use tokio::time::Instant;
-
-use ratchet_rs::{subscribe, WebSocketConfig};
+use utils::{await_server_start, cargo_command, validate_results};
 
 const PWD_ERR: &str = "Failed to get PWD";
-
-fn cargo_command() -> Result<Command> {
-    let mut pwd = current_dir().context(PWD_ERR)?;
-    pwd.push("ratchet_rs");
-
-    let mut cmd = Command::new("cargo");
-    cmd.args(&[
-        "run",
-        "--release",
-        "--example",
-        "autobahn-server",
-        "--features",
-        "deflate",
-    ])
-    .current_dir(pwd);
-
-    Ok(cmd)
-}
 
 fn docker_command() -> Result<Command> {
     let mut pwd = current_dir().context(PWD_ERR)?;
@@ -64,7 +39,7 @@ fn docker_command() -> Result<Command> {
 #[tokio::main]
 async fn main() -> Result<()> {
     let _server_process = tokio::spawn(async move {
-        cargo_command()
+        cargo_command("autobahn-server")
             .expect(PWD_ERR)
             .spawn()
             .expect("Failed to spawn autobahn server")
@@ -72,7 +47,7 @@ async fn main() -> Result<()> {
             .await
     });
 
-    await_server_start().await?;
+    await_server_start(9002).await?;
 
     let result = docker_command()?
         .spawn()
@@ -85,76 +60,10 @@ async fn main() -> Result<()> {
         bail!("Autobahn suite failed");
     }
 
-    validate_results()?;
+    let mut results_dir = current_dir().context(PWD_ERR)?;
+    results_dir.push("ratchet_rs/autobahn/server/results");
 
-    Ok(())
-}
-
-async fn await_server_start() -> Result<()> {
-    const TIMEOUT: u64 = 30;
-    let start = Instant::now();
-
-    loop {
-        match await_handshake().await {
-            Ok(()) => break Ok(()),
-            Err(_) => {
-                if start.elapsed() > Duration::from_secs(TIMEOUT) {
-                    bail!("Autobahn Server failed to start within {TIMEOUT} seconds");
-                }
-                tokio::time::sleep(Duration::from_secs(1)).await;
-            }
-        }
-    }
-}
-
-async fn await_handshake() -> std::result::Result<(), ratchet_rs::Error> {
-    let stream = TcpStream::connect("127.0.0.1:9002").await?;
-    subscribe(WebSocketConfig::default(), stream, "ws://127.0.0.1/hello")
-        .await
-        .map(|_| ())
-}
-
-fn validate_results() -> Result<()> {
-    let mut results_file = current_dir().context(PWD_ERR)?;
-    results_file.push("ratchet_rs/autobahn/server/results/index.json");
-
-    let file = File::open(results_file).context("Failed to open client results file")?;
-    let reader = BufReader::new(file);
-    let results = serde_json::from_reader::<_, Value>(reader)?;
-
-    match results {
-        Value::Object(map) => {
-            let ratchet_results = map["Ratchet"]
-                .as_object()
-                .expect("Invalid result structure");
-
-            let mut failures = Vec::new();
-
-            for (test_id, test) in ratchet_results {
-                match test {
-                    Value::Object(object) => match object["behavior"].as_str() {
-                        Some(result) if result == "OK" => {}
-                        Some(result) => {
-                            failures.push(format!("Test {test_id} failed with: {result}"));
-                        }
-                        _ => bail!("Invalid results structure"),
-                    },
-                    _ => bail!("Invalid results structure"),
-                }
-            }
-
-            if !failures.is_empty() {
-                eprintln!("Autobahn test suite encountered failures:");
-
-                for test in failures {
-                    eprintln!("\t{test}");
-                }
-
-                bail!("Autobahn test suite failure")
-            }
-        }
-        _ => bail!("Invalid results structure"),
-    }
+    validate_results(results_dir)?;
 
     Ok(())
 }
